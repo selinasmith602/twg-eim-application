c2ccdf527d50986a08eee48a611f7fc6d7a26ef3
TWG\302945 <wan.loke@thewarehouse.co.nz>
2018-02-07 14:56:03 +1300

    Updated TLSEmail.

--- src/main/java/nz/co/twg/eim/TLSEmail.java
+++ src/main/java/nz/co/twg/eim/TLSEmail.java
@@ -1,8 +1,5 @@
 package nz.co.twg.eim;
 
-import nz.co.twg.eim.dao.Notification;
-import nz.co.twg.eim.dao.NotificationInterface;
-
 import java.util.Date;
 import java.util.Properties;
 import javax.mail.Message;
@@ -13,62 +10,73 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 
+/**
+ Outgoing Mail (SMTP) Server
+ requires TLS or SSL: smtp.gmail.com (use authentication)
+ TLSEmail is using TSL connection via Gmail SMTP
+ Use Authentication: Yes
+ Port for TLS/STARTTLS: 587
+ */
+
+
 public class TLSEmail {
 
-    /**
-     Outgoing Mail (SMTP) Server
-     requires TLS or SSL: smtp.gmail.com (use authentication)
-     TLSEmail is using TSL connection via Gmail SMTP
-     Use Authentication: Yes
-     Port for TLS/STARTTLS: 587
-     */
-    public void emailNotification(String fileLocation, String conditionName){
+    /*NotificationInterface retrieveInfo = new Notification();
+    final String fromEmail = retrieveInfo.getFromEmail(fileLocation, conditionName);
+    final String fromEmailPassword = retrieveInfo.getFromEmailPassword(fileLocation, conditionName);
+    final String toEmail = retrieveInfo.getToEmail(fileLocation, conditionName);
+    String subjectLine = retrieveInfo.getEmailSubject(fileLocation, conditionName);
+    String emailBodyContent = retrieveInfo.getEmailBody(fileLocation, conditionName);*/
 
-        NotificationInterface retrieveInfo = new Notification();
-        final String fromEmail = retrieveInfo.getFromEmail(fileLocation, conditionName);
-        final String fromEmailPassword = retrieveInfo.getFromEmailPassword(fileLocation, conditionName);
-        System.out.println(fromEmail + fromEmailPassword);
-        final String toEmail = retrieveInfo.getToEmail(fileLocation, conditionName);
-        String subjectLine = retrieveInfo.getEmailSubject(fileLocation, conditionName);
-        String emailBodyContent = retrieveInfo.getEmailBody(fileLocation, conditionName);
+    private String senderEmail = "devtestingtwg@gmail.com";
+    private String senderEmailPassword = "discoduck";
+    private String userEmail = "wan.loke@thewarehouse.co.nz";
+    private String subjectLine = "Testing testing";
+    private String emailBodyContent = "Message sent!";
+    private String host = "smtp.gmail.com";
+    private int port=587;
+    private boolean auth=true;
+    private boolean starttls = true;
 
+    private Session getSession(){
         Properties setUpProps = new Properties();
-        setUpProps.put("mail.smtp.host", "smtp.gmail.com");
-        setUpProps.put("mail.smtp.port", "587");
-        setUpProps.put("mail.smtp.auth", "true");
-        setUpProps.put("mail.smtp.starttls.enable", "true");
-
+        setUpProps.put("mail.smtp.host", host);
+        setUpProps.put("mail.smtp.port", port);
+        setUpProps.put("mail.smtp.auth", auth);
+        setUpProps.put("mail.smtp.starttls.enable", starttls);
         //create Authenticator object to pass in Session.getInstance argument
         Authenticator auth = new Authenticator() {
             //override the getPasswordAuthentication method
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(fromEmail, fromEmailPassword);
+                return new PasswordAuthentication(senderEmail, senderEmailPassword);
             }
         };
         Session session = Session.getInstance(setUpProps, auth);
-        sendEmail(session, toEmail,subjectLine, emailBodyContent);
+        return session;
+    }
 
+    public void emailNotification(String fileLocation, String conditionName){
+        sendEmail(userEmail,subjectLine, emailBodyContent);
     }
 
     /**
      * Utility method to send simple HTML email
-     * @param session
      * @param toEmail
      * @param subject
      * @param body
      */
 
-    public static void sendEmail(Session session, String toEmail, String subject, String body){
+    public void sendEmail(String toEmail, String subject, String body){
         try
         {
-            MimeMessage message = new MimeMessage(session);
+            MimeMessage message = new MimeMessage(getSession());
 
             message.addHeader("Content-type", "text/HTML; charset=UTF-8");
             message.addHeader("format", "flowed");
             message.addHeader("Content-Transfer-Encoding", "8bit");
 
-            message.setFrom(new InternetAddress("devtestingtwg@gmail.com", "NoReply-MonitoringApp"));
-            message.setReplyTo(InternetAddress.parse("wan.loke@thewarehouse.co.nz", false));
+            message.setFrom(new InternetAddress(senderEmail, "NoReply-MonitoringApp"));
+            message.setReplyTo(InternetAddress.parse(userEmail, false));
             message.setSubject(subject, "UTF-8");
             message.setText(body, "UTF-8");
             message.setSentDate(new Date());
